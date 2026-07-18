6891f6e50390c89def1f38426369118bb852bf4a
Cooperwd01 <willdcooper@gmail.com>
2026-07-18 22:36:03 +0100

    automate image generation

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,3 +1,6 @@
+import Image from "@11ty/eleventy-img";
+import fs from "node:fs";
+
 export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/images");
@@ -10,8 +13,38 @@ export default function (eleventyConfig) {
     }).format(date);
   });
 
+  eleventyConfig.addFilter("eventTicketImage", function (slug) {
+    if (!slug) {
+      return "";
+    }
+
+    const extensions = [".jpg", ".jpeg", ".png", ".webp"];
+
+    const hasOriginal = extensions.some((extension) =>
+      fs.existsSync(`src/images/originals/${slug}${extension}`)
+    );
+
+    if (!hasOriginal) {
+      return "";
+    }
+
+    return `/images/generated/${slug}-400.webp`;
+  });
+
   eleventyConfig.addFilter("dateToISO", (date) => {
     return date.toISOString().slice(0, 10);
+  });
+
+  eleventyConfig.addFilter("hasEventImage", function (slug) {
+    if (!slug) {
+      return false;
+    }
+
+    const extensions = [".jpg", ".jpeg", ".png", ".webp"];
+
+    return extensions.some((extension) =>
+      fs.existsSync(`src/images/originals/${slug}${extension}`)
+    );
   });
 
   eleventyConfig.addCollection("eventsByDate", (collectionApi) => {
@@ -40,6 +73,56 @@ export default function (eleventyConfig) {
 
   return groupedEvents;
 });
+
+eleventyConfig.addAsyncShortcode(
+  "eventImage",
+  async function (src, variant = "ticket") {
+    const width = variant === "hero" ? 1200 : 400;
+   
+    const extensions = [".jpg", ".jpeg", ".png", ".webp"];
+
+    let sourceImage;
+
+    for (const extension of extensions) {
+      const candidate = `src/images/originals/${src}${extension}`;
+
+      if (fs.existsSync(candidate)) {
+        sourceImage = candidate;
+        break;
+      }
+    }
+
+    if (!sourceImage) {
+      return "";
+    }
+
+    const metadata = await Image(sourceImage, {
+      widths: [400, 1200],
+      formats: ["webp"],
+      outputDir: "_site/images/generated",
+      urlPath: "/images/generated",
+
+      filenameFormat: function (id, sourcePath, outputWidth, format) {
+        return `${src}-${outputWidth}.${format}`;
+      }
+    });
+
+    const className =
+    variant === "hero"
+    ? "event-hero-image"
+    : "event-ticket-image";
+
+    return Image.generateHTML(metadata, {
+      alt: "",
+      sizes: variant === "hero"
+        ? "(min-width: 55rem) 55rem, 100vw"
+        : "400px",
+      loading: variant === "hero" ? "eager" : "lazy",
+      decoding: "async",
+      class: className
+    });
+  }
+);
 
   return {
     dir: {
